8d3466fb0c7a3e73e3d2a00e92c69e477840ea13
atushi3110 <atushi.saito.busi@gmail.com>
2026-08-23 23:40:14 +0900

    Initial commit: Add UE5 project

---
+++ VolumetricFog/Source/VolumetricFog/VolumetricFog.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VolumetricFog.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VolumetricFog, "VolumetricFog" );
